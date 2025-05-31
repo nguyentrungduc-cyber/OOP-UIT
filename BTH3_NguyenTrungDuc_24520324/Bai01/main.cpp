f2527095cbd390c93b872ed2acfb3b05ef3756bb
Trung Duc <122720104+trungduccoder@users.noreply.github.com>
2025-05-31 09:12:31 +0700

    Add files via upload

---
+++ BTH3_NguyenTrungDuc_24520324/Bai01/main.cpp
@@ -0,0 +1,14 @@
+#include "hocsinh.h"
+
+int main()
+{
+    HocSinh No1;
+    No1.Nhap();
+
+    No1.KiemTraHoTen();
+    No1.KiemTraMSSV();
+    No1.KiemTraSDT();
+    No1.KiemTraDTB();
+
+    return 0;
+}
